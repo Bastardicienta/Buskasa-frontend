c7a8c1945daea8ec0944dc78aed5a36a7123be41
bastardicienta <latataalvarez1089@hotmail.com>
2022-02-23 19:59:23 -0500

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponentComponent } from './components/login-component/login-component.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'app-root', pathMatch:'full'},
+  {path:"principal", component: PrincipalComponent},
+  {path:"login", component: LoginComponentComponent},
+  {path:'**', redirectTo:'app-root', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApartmentsComponent } from './components/apartments/apartments.component';
+import { LoginComponentComponent } from './components/login-component/login-component.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApartmentsComponent,
+    LoginComponentComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
